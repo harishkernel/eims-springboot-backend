b4f1ffe5282d4cb0eebfd61d3e212235cb5d8f48
harishkernel <harishmuthaiyan05@gmail.com>
2026-06-29 18:21:07 +0530

    Initial commit

---
+++ src/main/java/com/harish/eimsspringbootbackend/EimsSpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.harish.eimsspringbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EimsSpringbootBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EimsSpringbootBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/harish/eimsspringbootbackend/EimsSpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harish.eimsspringbootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EimsSpringbootBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
